797b043c0268e418f3a16000ea8774602e1116fc
muhammadriqi <142365175+muhammadriqi@users.noreply.github.com>
2024-03-19 12:28:05 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar bg-primary">
+    <div class="container-fluid">
+      <a class="brand-navbar" href="#">
+        <img src="~/assets/img/logo smk.png" alt="Bootstrap" width="80">
+      </a>
+      <div class="title me-auto">
+        <h1 class="m-0">Perpus Digital</h1>
+        <address class="m-0">
+          SMKN 4 Tasikmalaya<br>
+          Jl.Depok, Sukamenak, Purbaratu
+        </address>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ pages/rincian_buku/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container p-2 mt-3">
+    <div class="row mt-4">
+      <div class="col-3">
+        <img src="~/assets/img/cover 2.jpg" class="cover" alt="cover 1" width="270px">
+      </div>
+      <div class="col-4">
+        <h2>Rincian Buku</h2>
+        <p>Judul buku : hikayat majapahit</p>
+        <p>Penulis :elex media </p>
+        <p>Tahun terbit :11 juni 2020 </p>
+        <p>Halaman : 252</p>
+        <p>Kategori : Dongeng</p>
+        <p>ISBN : 978 634 567 890</p>
+      </div>
+    </div>
+    <div class="row">
+      <p>Deskripsi Buku
+Kerajaan Majapahit berdiri antara tahun 1293 hingga sekitar 1527, kerajaan yang berpusat di daerah Jawa Timur ini mencapai puncak kejayaannya pada saat di bawah pemerintahan Prabu Hayam Wuruk. Beliau ini yang memperluas wilayah kerajaannya di kepulauan Nusantara dan daratan Asia Tenggara. Keberhasilannya tersebut dicapai lewat bantuan mahapatihnya,</p>
+    </div>
+    <nuxt-link to="/buku/">
+      <button type="button" class="btn btn-secondary" disabled>KEMBALI</button>
+    </nuxt-link>
+  </div>
+</template>
